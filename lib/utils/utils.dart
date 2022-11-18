112c7acd94dca5015f075023841943b915d02f63
Fenyep <0Uiimi1D5/-yYkGt>
2022-11-18 06:11:47 +0100

    Added some utils functions for displaying spacements

---
+++ lib/utils/utils.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Widget addHorizontalSpace(double size) {
+  return SizedBox(
+    width: size,
+  );
+}
+
+Widget addVerticalSpace(double size) {
+  return SizedBox(
+    height: size,
+  );
+}
