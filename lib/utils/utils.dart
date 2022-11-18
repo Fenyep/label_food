00b0c2b47d928b649c99b13e7e3346c970f37a80
Fenyep <0Uiimi1D5/-yYkGt>
2022-11-18 07:37:52 +0100

    added the app general color constants

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,12 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
+
+const Color COLOR_PRIMARY = Color(0xFFFF8A3D);
+const Color COLOR_BLACK = Color(0xFF000000);
+const Color COLOR_WHITE = Color(0xFFFFFFFF);
+const Color COLOR_GREY = Color(0xFFD9D9D9);
+const Color COLOR_LIGHT_BLUE = Color(0xFF5470FF);
 
 Widget addHorizontalSpace(double size) {
   return SizedBox(
