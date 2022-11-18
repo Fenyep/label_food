001b694d9c26cd8e8b8b3217777b31ef4b172cbb
Fenyep <0Uiimi1D5/-yYkGt>
2022-11-18 07:41:02 +0100

    feature-test: connected the label_food app to the testing logic

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:label_food/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LabelFood());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
